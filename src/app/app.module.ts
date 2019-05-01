4276b028cc21fea5e4adf841d00b2dd5917c578b
unknown <paulo.vfcamara@gmail.com>
2019-04-30 22:07:31 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Homem de ferro' },
+    { id: 2, name: 'Thor' },
+    { id: 3, name: 'Homem Formiga' },
+    { id: 4, name: 'Homem Aranha' },
+    { id: 5, name: 'Capitã Marvel' },
+    { id: 6, name: 'Hulk' },
+    { id: 7, name: 'Capitão América' },
+]
